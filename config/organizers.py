6c3af6132a8d37741a8b44bfd7dab9b47ccd871c
Terry Yanchynskyy <tarasics@gmail.com>
2017-07-15 21:59:18 -0400

    separate organizers config

---
+++ config/organizers.py
@@ -0,0 +1,10 @@
+
+ORGANIZERS = {
+    ('Francis Deslauriers', 'https://twitter.com/francisDeslaur'),
+    ('Myles Braithwaite', 'https://mylesb.ca/'),
+    ('Peter McCormick', 'https://twitter.com/pdmccormick'),
+    ('Terry Yanchynskyy', 'https://github.com/onebit0fme'),
+
+    # Add you name and url here ^ and submit a pull request
+    # Order does not matter, final result is sorted by name
+}
--- pyconca2017/pycon_proposals/context_processors.py
+++ pyconca2017/pycon_proposals/context_processors.py
@@ -1,18 +1,12 @@
 from symposion.conference.models import current_conference
+from config.organizers import ORGANIZERS
 
-organizers = (
-    ('Terry Yanchynskyy', 'https://github.com/onebit0fme'),
-    ('Myles Braithwaite', 'https://mylesb.ca/'),
-    ('Francis Deslauriers', 'https://twitter.com/francisDeslaur'),
-    ('Peter McCormick', 'https://twitter.com/pdmccormick'),
-)
-
-organizers = sorted(organizers, key=lambda x: x[0])
+_organizers = sorted(ORGANIZERS, key=lambda x: x[0])
 
 
 def conference_context(request):
 
     return {
         "CONFERENCE": current_conference(),
-        "ORGANIZERS": organizers,
+        "ORGANIZERS": _organizers,
     }
