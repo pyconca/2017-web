42798c8938cd4b2bbf64c858c8aab613c0bc9e9c
Terry Yanchynskyy <tarasics@gmail.com>
2017-07-20 23:18:54 -0400

    deploy .env file

--- utility/scripts/gunicorn_run.sh
+++ utility/scripts/gunicorn_run.sh
@@ -6,23 +6,12 @@ SOCKFILE={{ run_root }}/gunicorn.sock
 USER=deploy
 GROUP=deploy
 NUM_WORKERS={{ workers }}
-DJANGO_SETTINGS_MODULE=config.settings.production
 DJANGO_WSGI_MODULE=config.wsgi
-DATABASE_URL=postgres://{{ db_user }}:{{ db_pass }}@localhost:5432/{{ db_name }}
-SLACKBOT_TOKEN={{ slackbot_token }}
 
 # Activate the virtual environment
 cd $DJANGODIR
 source ../env/bin/activate
-export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
-export DATABASE_URL=$DATABASE_URL
-export PYTHONPATH=$DJANGODIR:$PYTHONPATH
-export SLACKBOT_TOKEN='$SLACKBOT_TOKEN'
-export DJANGO_ADMIN_URL=admin
-export DJANGO_ALLOWED_HOSTS='{{ allowed_hosts }}'
-export DJANGO_SECRET_KEY='{{ django_secret_key }}'
-export DJANGO_DEBUG=1
-export DJANGO_SECURE_SSL_REDIRECT=0
+export `cat ../run/.env | xargs`
 
 # Create the run directory if it doesn't exist
 RUNDIR=$(dirname $SOCKFILE)
