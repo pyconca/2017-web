442974156c9749a5fee6bb2b85658bd731e044dd
Terry Yanchynskyy <tarasics@gmail.com>
2017-09-16 00:10:26 -0400

    Schedule redirect view; schedule "tab" links

--- pyconca2017/pycon_schedule/urls.py
+++ pyconca2017/pycon_schedule/urls.py
@@ -4,8 +4,9 @@ from __future__ import absolute_import, unicode_literals
 from django.conf.urls import url
 from django.contrib.admin.views.decorators import staff_member_required
 
-from pyconca2017.pycon_schedule.views import SchedulePreview
+from pyconca2017.pycon_schedule.views import ScheduleView, ScheduleRedirectView
 
 urlpatterns = [
-    url(r'^preview$', staff_member_required(SchedulePreview.as_view()), name='preview'),
+    url(r'^$', ScheduleRedirectView.as_view(), name='current'),
+    url(r'^(?P<schedule_date>\d{4}-\d{2}-\d{2})$', staff_member_required(ScheduleView.as_view()), name='detail'),
 ]
