557173257d43b75382dab40e3cbfcf9929e1ee75
Terry Yanchynskyy <tarasics@gmail.com>
2017-10-03 21:19:42 -0400

    because rollback if dirty import

--- pyconca2017/pycon_schedule/forms.py
+++ pyconca2017/pycon_schedule/forms.py
@@ -2,6 +2,7 @@ import csv
 from io import StringIO
 
 from django import forms
+from django.db import transaction
 
 from pyconca2017.pycon_schedule.models import Location, Presentation, SlotEvent
 
@@ -14,23 +15,24 @@ class AssignCSVForm(forms.Form):
         decoded_file = file.read().decode('utf-8')
         io_string = StringIO(decoded_file)
         reader = csv.DictReader(io_string, dialect=csv.excel)
-        for row in reader:
-            ref = row['Ref']
-            for track in ['Track 1', 'Track 2', 'Track 3', 'Tutorial']:
-                track_slug = {
-                    'Track 1': Location.Tracks.TRACK_ONE,
-                    'Track 2': Location.Tracks.TRACK_TWO,
-                    'Track 3': Location.Tracks.TRACK_THREE,
-                    'Tutorial': Location.Tracks.TRACK_TUTORIAL,
-                }[track]
-                presentation_id = row[track]
-                if presentation_id:
-                    try:
-                        presentation = Presentation.objects.get(papercall_id=presentation_id)
-                    except Presentation.DoesNotExist:
-                        # TODO: not synced
-                        continue
-                    loc = Location.objects.get(track=track_slug)
-                    event = SlotEvent.objects.get(location=loc, slot__ref=ref)
-                    event.presentation = presentation
-                    event.save()
+        with transaction.atomic():
+            for row in reader:
+                ref = row['Ref']
+                for track in ['Track 1', 'Track 2', 'Track 3', 'Tutorial']:
+                    track_slug = {
+                        'Track 1': Location.Tracks.TRACK_ONE,
+                        'Track 2': Location.Tracks.TRACK_TWO,
+                        'Track 3': Location.Tracks.TRACK_THREE,
+                        'Tutorial': Location.Tracks.TRACK_TUTORIAL,
+                    }[track]
+                    presentation_id = row[track]
+                    if presentation_id:
+                        try:
+                            presentation = Presentation.objects.get(papercall_id=presentation_id)
+                        except Presentation.DoesNotExist:
+                            # TODO: not synced
+                            continue
+                        loc = Location.objects.get(track=track_slug)
+                        event = SlotEvent.objects.get(location=loc, slot__ref=ref)
+                        event.presentation = presentation
+                        event.save()
